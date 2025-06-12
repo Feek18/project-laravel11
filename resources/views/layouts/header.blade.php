d90514cd5fdc0b0d1971a7309b3df11bf0e3162f
Feek18 <bintangfikri31@gmail.com>
2025-06-12 20:17:44 +0800

    update header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -29,7 +29,7 @@
                     <a href="#"
                         class="block py-2 px-3 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 md:dark:hover:bg-transparent">Tentang</a>
                 </li>
-                <li>
+                {{-- <li>
                     <a href="{{ route('qr.test') }}" target="_blank"
                         class="block py-2 px-3 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 md:dark:hover:bg-transparent">
                         <svg class="w-4 h-4 inline mr-1" fill="currentColor" viewBox="0 0 20 20">
@@ -37,7 +37,7 @@
                         </svg>
                         QR Test
                     </a>
-                </li>
+                </li> --}}
                 {{-- <li>
                     <a href="{{ route('qr.conflict.demo') }}" target="_blank"
                         class="block py-2 px-3 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 md:dark:hover:bg-transparent">
